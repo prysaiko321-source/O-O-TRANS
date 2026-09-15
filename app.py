44a9f0469418123c386c9d9c17fc99645545d656
prysaiko321-source <prysaiko321@gmail.com>
2026-09-15 16:13:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "O&O TRANS bot працює!"
+    token = os.getenv("NAVIREC_TOKEN")
+
+    if token:
+        return "O&O TRANS bot працює! NAVIREC_TOKEN отримано."
+    else:
+        return "O&O TRANS bot працює, але NAVIREC_TOKEN НЕ знайдено."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
